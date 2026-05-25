9a55d0679b3ce9ff1cb7c850df5f6050cf8e75e1
mappy <mappy3479@gmail.com>
2026-05-26 06:53:52 +0900

    入力欄とボタンの作成

--- apps/login-practice/app/page.tsx
+++ apps/login-practice/app/page.tsx
@@ -2,6 +2,8 @@ export default function page() {
   return (
     <>
       <h1>HelloNext.js</h1>
+      <input name="myinput" type="email" />
+      <input type="submit" value="リクエストを送信" />
     </>
   );
 }
