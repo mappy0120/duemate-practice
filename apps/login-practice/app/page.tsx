4d304647bb394c17fee43217ac224770ec4ee313
mappy <mappy3479@gmail.com>
2026-06-07 21:44:10 +0900

    fix: add use client directive

--- apps/login-practice/app/page.tsx
+++ apps/login-practice/app/page.tsx
@@ -1,9 +1,18 @@
-export default function page() {
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [email, setEmail] = useState('sample@email.com');
+  const [password, setPassword] = useState('samplepassword0123');
+
   return (
-    <>
-      <h1>HelloNext.js</h1>
-      <input name="myinput" type="email" />
+    <form>
+      <h1>メール送信アプリ！</h1>
+      <h2>※メールを送信するだけのアプリです。</h2>
+
+      <input name="email" type="email" />
       <input type="submit" value="リクエストを送信" />
-    </>
+    </form>
   );
 }
