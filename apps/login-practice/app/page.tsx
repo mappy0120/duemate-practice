f2d558880f6cec044faaf734ce7d0bfa315cac29
mappy <mappy3479@gmail.com>
2026-06-25 06:49:12 +0900

    最小限のコードに変更

--- apps/login-practice/app/page.tsx
+++ apps/login-practice/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useEffect, useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 export default function Page() {
-  const [email, setEmail] = useState('sample@email.com');
-  const [password, setPassword] = useState('samplepassword0123');
-  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
     console.log(e.target.value);
   };
@@ -14,7 +14,12 @@ export default function Page() {
       <h1>メール送信アプリ！</h1>
       <h2>※メールを送信するだけのアプリです。</h2>
 
-      <input name="email" type="email" />
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
       <input type="submit" value="リクエストを送信" />
     </form>
   );
